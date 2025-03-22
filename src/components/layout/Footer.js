d829f1463f41379a3063576c5f1c833bfe906418
Karthik <karthik.nishanth06@gmail.com>
2025-03-22 15:36:01 +0530

    add

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white pt-12 pb-6">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Company Info */}
           <div>
             <h3 className="text-xl font-bold mb-4">DSN Enterprises</h3>
